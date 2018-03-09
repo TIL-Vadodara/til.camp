220f303896cd9357c1a3369d54d7dc2ef35fdc95
Nayaabkhan Khan <khannayaab@gmail.com>
2018-03-09 20:06:53 +0530

    [til-v2] Fetch past events from Firebase

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import classNames from './index.scss'
-import whatsappImage from './whatsapp.svg'
-import meetupImage from './meetup.svg'
-import instagramImage from './instagram.svg'
+import { InstagramLogo, MeetupLogo, WhatsAppLogo } from '../Icons'
+// import whatsappImage from './whatsapp.svg'
+// import meetupImage from './meetup.svg'
+// import instagramImage from './instagram.svg'
 
 const Join = () => (
   <div>
@@ -10,13 +11,13 @@ const Join = () => (
 
     <div className={classNames.channelList}>
       <a className={classNames.link} href="https://www.meetup.com/TIL-Today-I-Learn/" target="_blank">
-        <img alt="meetup" src={meetupImage} />
+        <MeetupLogo alt="meetup" />
       </a>
       <a className={classNames.link} href="https://chat.whatsapp.com/5QjZq084FmHDXsrRfXKAj1" target="_blank">
-        <img alt="whatsapp" src={whatsappImage} />
+        <WhatsAppLogo alt="whatsapp" />
       </a>
       <a className={classNames.link} href="https://www.instagram.com/til.camp/" target="_blank">
-        <img alt="instagram" src={instagramImage} />
+        <InstagramLogo alt="instagram" />
       </a>
     </div>
   </div>
