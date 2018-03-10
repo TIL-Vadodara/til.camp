097328cd49542393b03031b5fc36d2315a2874ac
Nayaabkhan Khan <khan@starofservice.com>
2018-03-10 13:44:12 +0530

    [til-v2] Highlight active links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import * as routes from '../../constants/routes'
 import Logo from '../Logo'
 import classNames from './index.scss'
@@ -12,16 +12,16 @@ const Header = () => (
 
     <div className={classNames.navigation}>
       <div className={classNames.navigationItem}>
-        <Link to={routes.HOME}>Next Event</Link>
+        <NavLink exact to={routes.HOME} className={classNames.link} activeClassName={classNames.linkActive}>Next Event</NavLink>
       </div>
       <div className={classNames.navigationItem}>
-        <Link to={routes.PAST_EVENTS}>Past Events</Link>
+        <NavLink to={routes.PAST_EVENTS} className={classNames.link} activeClassName={classNames.linkActive}>Past Events</NavLink>
       </div>
       <div className={classNames.navigationItem}>
-        <Link to={routes.JOIN}>Join us</Link>
+        <NavLink to={routes.JOIN} className={classNames.link} activeClassName={classNames.linkActive}>Join us</NavLink>
       </div>
       <div className={classNames.navigationItem}>
-        <Link to={routes.ABOUT}>About TIL</Link>
+        <NavLink to={routes.ABOUT} className={classNames.link} activeClassName={classNames.linkActive}>About TIL</NavLink>
       </div>
     </div>
   </div>
