d058fa686c4e674d1fc2fd5c3be9f368c059f46c
Nayaabkhan Khan <khannayaab@gmail.com>
2018-06-25 18:49:01 +0530

    Add plugin for styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'TIL: Today I Learned',
   },
   plugins: [
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-gsheets',
